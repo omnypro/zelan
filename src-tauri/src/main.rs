f86ed41266a6e0fe667eab43f307c31210afafa9
Bryan Veloso <bryan@velo.so>
2025-02-24 15:25:29 -0800

    Add the `single-instance` plugin.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
 use zelan_lib::plugin;
 
 fn main() {
     // Build and run the Tauri application with our plugin
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app.get_webview_window("main")
+                       .expect("no main window")
+                       .set_focus();
+        }))
         .plugin(tauri_plugin_opener::init())
         .plugin(plugin::init())
         .setup(|_app| {
