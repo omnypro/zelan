b71b8134cf3c45f804e4da71fd0c046df80f2f26
Bryan Veloso <bryan@velo.so>
2025-03-01 18:47:51 -0800

    Handle environment varibles.

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,35 +1,44 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, loadEnv } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()],
-    resolve: {
-      alias: {
-        '@main': resolve('src/main'),
-        '@shared': resolve('src/shared'),
-        '@preload': resolve('src/preload')
-      }
-    }
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()],
-    resolve: {
-      alias: {
-        '@shared': resolve('src/shared'),
-        '@preload': resolve('src/preload')
-      }
-    }
-  },
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src'),
-        '@shared': resolve('src/shared'),
-        '@preload': resolve('src/preload')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    main: {
+      define: {
+        'process.env.TWITCH_CLIENT_ID': JSON.stringify(
+          env.TWITCH_CLIENT_ID || process.env.TWITCH_CLIENT_ID
+        )
+      },
+      plugins: [externalizeDepsPlugin()],
+      resolve: {
+        alias: {
+          '@main': resolve('src/main'),
+          '@shared': resolve('src/shared'),
+          '@preload': resolve('src/preload')
+        }
       }
     },
-    plugins: [react()]
+    preload: {
+      plugins: [externalizeDepsPlugin()],
+      resolve: {
+        alias: {
+          '@shared': resolve('src/shared'),
+          '@preload': resolve('src/preload')
+        }
+      }
+    },
+    renderer: {
+      resolve: {
+        alias: {
+          '@renderer': resolve('src/renderer/src'),
+          '@shared': resolve('src/shared'),
+          '@preload': resolve('src/preload')
+        }
+      },
+      plugins: [react()]
+    }
   }
 })
