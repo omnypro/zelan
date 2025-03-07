3db5a6f23ed7fecafb43079444f431bf44b998a8
Bryan Veloso <bryan@velo.so>
2025-03-07 10:12:37 -0800

    Copypasta.

---
+++ src/views/home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function HomeView() {
+  return (
+    <div className="flex h-full flex-col bg-zinc-950">
+    </div>
+  );
+}
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,13 @@
-import { defineConfig } from 'vite';
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    preserveSymlinks: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  clearScreen: false,
+});
